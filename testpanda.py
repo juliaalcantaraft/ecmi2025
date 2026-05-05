380f822806073677e25bae882059a1dfa66ca035
juliaalcantaraft <julia.alcantaraft@gmail.com>
2026-05-05 15:25:54 -0300

    Update testpanda.py

--- testpanda.py
+++ testpanda.py
@@ -7,7 +7,7 @@ sigla = st.text_input('Digite a Sigla do Partido')
 uf = st.text_input('Digite a UF')
 
 if sigla:
-  df_filtrado = df[df['partido'] == sigla.upper()]
+   df_filtrado = df[df['partido'] == sigla.upper()]
 else:
     df_filtrado = df
 
@@ -15,4 +15,9 @@ if uf:
    df_filtrado = df_filtrado[df_filtrado['uf'] == uf.upper()]
 
 st.dataframe(df_filtrado)
+
+# Gráfico
+st.subheader('Deputados por Partido')
+grafico = df_filtrado['partido'].value_counts()
+st.bar_chart(grafico)
 
