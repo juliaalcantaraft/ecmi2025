470bf49540829235f716c33f40cbb23ee42f6ca8
juliaalcantaraft <julia.alcantaraft@gmail.com>
2026-05-05 15:20:08 -0300

    Update testpanda.py

--- testpanda.py
+++ testpanda.py
@@ -7,12 +7,12 @@ sigla = st.text_input('Digite a Sigla do Partido')
 uf = st.text_input('Digite a UF')
 
 if sigla:
-df_filtrado = df[df['partido'] == sigla.upper()]
+  df_filtrado = df[df['partido'] == sigla.upper()]
 else:
-df_filtrado = df
+    df_filtrado = df
 
 if uf:
-df_filtrado = df_filtrado[df_filtrado['uf'] == uf.upper()]
+   df_filtrado = df_filtrado[df_filtrado['uf'] == uf.upper()]
 
 st.dataframe(df_filtrado)
 
