059135e1bf302747943be6783a21e31ecdb24079
juliaalcantaraft <julia.alcantaraft@gmail.com>
2026-05-05 15:05:22 -0300

    Update testpanda.py

--- testpanda.py
+++ testpanda.py
@@ -4,6 +4,11 @@ import pandas as pd
 df = pd.read_csv('deputados_2022.csv')
 
 st.title("Deputados 2022")
-st.dataframe(df[['nome', 'uf', 'partido']])
+
+uf = st.text_input("Digite a sigla dos deputados:")
+
+if uf:
+filtrado = df[df['uf'] == uf.upper()]
+st.dataframe(filtrado[['nome', 'uf', 'partido']])
 
 
